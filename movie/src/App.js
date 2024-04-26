52c471b0c746da6f594749823f1a0c210eee3456
NamanChadha <156222435+NamanChadha@users.noreply.github.com>
2024-04-26 15:37:25 +0530

    new adjustments by naman

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react';
 import './App.css';
 import MovieCard from './MovieCard';
 import Login from './Login';
@@ -17,7 +17,6 @@ const ProtectedPage = ({ onLogout }) => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
-        // Add a rating property set to 0 for each movie object
         const moviesWithRating = data.results.map(movie => ({
           ...movie,
           rating: 0
@@ -33,7 +32,6 @@ const ProtectedPage = ({ onLogout }) => {
     fetch(API_SEARCH + searchTerm)
       .then((res) => res.json())
       .then(data => {
-        // Add a rating property set to 0 for each movie object
         const moviesWithRating = data.results.map(movie => ({
           ...movie,
           rating: 0
@@ -43,16 +41,13 @@ const ProtectedPage = ({ onLogout }) => {
   };
 
   const handleAddToWatchlist = (movieId) => {
-    // Check if the movie is already in the watchlist
     if (!watchlist.some(movie => movie.id === movieId)) {
-      // If not, find the movie in the movies array and add it to the watchlist
       const movieToAdd = movies.find(movie => movie.id === movieId);
       setWatchlist([...watchlist, movieToAdd]);
     }
   };
 
   const handleRemoveFromWatchlist = (movieId) => {
-    // Filter out the movie with the specified ID from the watchlist
     const updatedWatchlist = watchlist.filter(movie => movie.id !== movieId);
     setWatchlist(updatedWatchlist);
   };
@@ -74,14 +69,24 @@ const ProtectedPage = ({ onLogout }) => {
 
   return (
     <div className="App">
-      <button onClick={onLogout}>Logout</button>
-
+      <header className="header">
+      <div className="logo">
+        <img src="./src/bingebuddy.png" alt="Logo" />
+      </div>
+      <div className="navigation">
+        <button>Home</button>
+        <button onClick={() => setShowWatchlist(true)}>Watchlist</button>
+        <button>Profile</button>
+        <button style={{ backgroundColor: '#d35400' }} onClick={onLogout}>Logout</button>
+      </div>
       <div className='form'>
         <form onSubmit={handleSubmit}>
           <input onChange={(e) => setSearchTerm(e.target.value)} />
           <button type='submit'>Submit</button>
         </form>
       </div>
+    </header>
+
 
       <div className='appContainer'>
         {movies.map(movie => (
@@ -96,9 +101,7 @@ const ProtectedPage = ({ onLogout }) => {
         ))}
       </div>
 
-      <button onClick={handleToggleWatchlist}>View Watchlist</button>
       {showWatchlist && <WatchlistPage watchlist={watchlist} />}
-
     </div>
   );
 };
@@ -111,7 +114,6 @@ const App = () => {
   };
 
   const handleLogout = () => {
-    console.log("Meow");
     setIsLoggedIn(false);
   }
 
@@ -127,3 +129,4 @@ const App = () => {
 };
 
 export default App;
+
